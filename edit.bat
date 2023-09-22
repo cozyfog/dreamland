eaa2de59f0118e26549c47878019582ed3345067
nickel-dev <nickel-dev@proton.me>
2023-09-22 16:25:53 +0200

    Fixed build script aswell as gitignore.

--- edit.bat
+++ edit.bat
@@ -1,5 +1,5 @@
 @echo off
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-start devenv build\win32_dreamland.sln
+start devenv win32_dreamland.sln
 start C:\4coder\4ed.exe project.4coder
 exit
